518a2580d62fa7a91c818cae9b2cb5301f792a8d
AgustinRafanelli <agustinrafa1995@gmail.com>
2023-08-18 12:05:46 -0300

    Mejora de validacion de datos

--- src/services/api-service.ts
+++ src/services/api-service.ts
@@ -3,16 +3,31 @@ import axios from "axios";
 interface AccountType {
   tipo_letras: string;
   moneda: string;
+  n: string;
+  saldo: string;
 }
 
-
-function fetchAccountsScript(cuentas: any) {
+function accountsFilter(cuentas: any) {
   return cuentas.filter((account: AccountType) => {
+    // valida que la moneda de la cuenta sea pesos o dolares
     const validCurrency: Set<string> = new Set(["$", "u$s"]);
+    // valida que el tipo de cuenta sea Caja de Ahorros o Cuenta Corriente
     const validAccountType: Set<string> = new Set(["CC", "CA"]);
+    // valida el numero de la cuenta sea un numero y sea mayor a 0
+    const validNumber = (number: string) => {
+      const regex = /^[1-9][0-9]*$/;
+      return regex.test(number);
+    };
+    // valida que el saldo de la cuenta sea un numero real
+    const validBalance = (balance: string) => {
+      const regex = /^-?(?:0|[1-9]\d*)$/;
+      return regex.test(balance)
+    };
     return (
       validCurrency.has(account.moneda) &&
-      validAccountType.has(account.tipo_letras)
+      validAccountType.has(account.tipo_letras) &&
+      validNumber(account.n) &&
+      validBalance(account.saldo)
     );
   });
 }
@@ -24,7 +39,7 @@ export function fetchAccounts() {
     url: url,
   })
     .then(({ data }) => {
-      return fetchAccountsScript(data.cuentas);
+      return accountsFilter(data.cuentas);
     })
     .catch((err) => console.error(err));
 }
